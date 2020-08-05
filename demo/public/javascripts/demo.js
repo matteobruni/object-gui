ccd89bc4616fb897696f2731bfdf5a92f7357109
Matteo Bruni <ar3s@icloud.com>
2020-08-05 15:50:07 +0200

    range slider

--- demo/public/javascripts/demo.js
+++ demo/public/javascripts/demo.js
@@ -1,11 +1,12 @@
 var code = document.getElementById("code");
 
 var data = {
-  prop1: "pluto",
-  group1: {
-    prop1: "paperino"
-  },
-  color1: "#ff0000"
+    prop1: "pluto",
+    prop2: 0,
+    group1: {
+        prop1: "paperino"
+    },
+    color1: "#ff0000"
 };
 var editor = new Editor("sample", "Sample", data);
 
@@ -15,46 +16,61 @@ editor.theme("light");
 var group1 = editor.root.addGroup("group1", "Group 1", false);
 
 group1.addProperty(
-  "group1_prop1",
-  "Property 1",
-  data.group1.prop1,
-  "string",
-  (value) => {
-    data.group1.prop1 = value;
-    code.innerText = JSON.stringify(data, null, 4);
+    "group1_prop1",
+    "Property 1",
+    data.group1.prop1,
+    "string",
+    (value) => {
+        data.group1.prop1 = value;
+        code.innerText = JSON.stringify(data, null, 4);
 
-    console.log(data);
-  }
+        console.log(data);
+    }
 );
 
 editor.root.addProperty(
-  "prop1",
-  "Property 1",
-  data.prop1,
-  "string",
-  (value) => {
-    data.prop1 = value;
-    code.innerText = JSON.stringify(data, null, 4);
+    "prop1",
+    "Property 1",
+    data.prop1,
+    "string",
+    (value) => {
+        data.prop1 = value;
+        code.innerText = JSON.stringify(data, null, 4);
 
-    console.log(data);
-  }
+        console.log(data);
+    }
 );
 
-editor.root.addProperty(
-  "color1",
-  "Color 1",
-  data.color1,
-  "color",
-  (value) => {
-    data.color1 = value;
-    code.innerText = JSON.stringify(data, null, 4);
+const prop2Input = editor.root.addProperty(
+    "prop2",
+    "Property 2",
+    data.prop2,
+    "number",
+    (value) => {
+        data.prop2 = value;
+        code.innerText = JSON.stringify(data, null, 4);
 
-    console.log(data);
-  }
+        console.log(data);
+    }
+);
+
+prop2Input.min(0).max(10).step(0.5);
+
+editor.root.addProperty(
+    "color1",
+    "Color 1",
+    data.color1,
+    "color",
+    (value) => {
+        data.color1 = value;
+        code.innerText = JSON.stringify(data, null, 4);
+
+        console.log(data);
+    }
 );
 
 editor.root.addButton("alert", "Alert", () => {
-  alert(JSON.stringify(data, null, 4));
+    alert(JSON.stringify(data, null, 4));
 });
 
 code.innerText = JSON.stringify(data, null, 4);
--- src/ts/Editors/EditorNumberInput.ts
+++ src/ts/Editors/EditorNumberInput.ts
@@ -1,6 +1,10 @@
 import { EditorItem } from "./EditorItem";
 
 export class EditorNumberInput extends EditorItem {
+    private _max?: number;
+    private _min?: number;
+    private _step?: number;
+
     constructor(
         data: unknown,
         private readonly name: string,
@@ -30,11 +34,17 @@ export class EditorNumberInput extends EditorItem {
     public step(step: number): EditorNumberInput {
         (this.element as HTMLInputElement).step = step.toString(10);
 
+        this._step = step;
+
         return this;
     }
 
     public min(min: number): EditorNumberInput {
         (this.element as HTMLInputElement).min = min.toString(10);
+
+        this._min = min;
+
+        this.drawSlider();
 
         return this;
     }
@@ -42,6 +52,94 @@ export class EditorNumberInput extends EditorItem {
     public max(max: number): EditorNumberInput {
         (this.element as HTMLInputElement).max = max.toString(10);
 
+        this._max = max;
+
+        this.drawSlider();
+
         return this;
+    }
+
+    private updateDragger(e: MouseEvent, down: boolean, rangeLeft: number, rangeWidth: number, dragger: HTMLElement) {
+        if (down && e.pageX >= rangeLeft && e.pageX <= rangeLeft + rangeWidth) {
+            const max = this._max ?? 0;
+            const min = this._min ?? 0;
+            const width = e.pageX - rangeLeft;
+            const value = (width / rangeWidth) * (max - min) + min;
+
+            dragger.style.width = `${width}px`;
+
+            this.onDrag(value);
+        }
+    }
+
+    private onDrag(value: number): void {
+        const input = this.element as HTMLInputElement;
+
+        input.value = value.toString(10);
+
+        this.value = value;
+
+        this.change(this.value);
+    }
+
+    private drawSlider(): void {
+        if (this._max === undefined || this._min === undefined) {
+            return;
+        }
+
+        const parent = this.element.parentElement;
+
+        if (!parent) {
+            return;
+        }
+
+        const range = parent.querySelector(".range-slider");
+
+        if (range) {
+            return;
+        }
+
+        const slider = document.createElement("div");
+
+        slider.classList.add("range-slider");
+
+        slider.appendChild(document.createElement("span"));
+
+        parent.insertBefore(slider, this.element);
+
+        const dragger = slider.children[0] as HTMLElement;
+
+        let down = false;
+        let rangeWidth: number;
+        let rangeLeft: number;
+
+        dragger.style.width = `${this.value}px`;
+        dragger.style.left = "0px";
+        dragger.style.marginLeft = "0px";
+
+        slider.addEventListener("mousedown", (e: Event) => {
+            if (!e.target) {
+                return;
+            }
+
+            const elem = e.target as HTMLElement;
+            const rect = elem.getBoundingClientRect();
+
+            rangeLeft = rect.left;
+            rangeWidth = elem.offsetWidth;
+            down = true;
+
+            this.updateDragger(e as MouseEvent, down, rangeLeft, rangeWidth, dragger);
+
+            return false;
+        });
+
+        document.addEventListener("mousemove", (e) => {
+            this.updateDragger(e, down, rangeLeft, rangeWidth, dragger);
+        });
+
+        document.addEventListener("mouseup", () => {
+            down = false;
+        });
     }
 }
