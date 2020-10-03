423f2abd4cbe5035c99ef1a4d038d20d9de1d9c8
redcreeper14385 <noahkoro@gmail.com>
2020-10-03 15:45:48 +0100

    Renamed themes (again :P)

--- src/ts/Editor.ts
+++ src/ts/Editor.ts
@@ -33,8 +33,8 @@ export class Editor {
         this.addTheme("neu-light");
         this.addTheme("red");
         this.addTheme("purple");
-        this.addTheme("light2");
-        this.addTheme("dark2");
+        this.addTheme("sunlight");
+        this.addTheme("moonlight");
         this.addTheme("orange");
 
         this.root = EditorGroup.createRoot(`${id}_editor`, name, data, document.body, this.themeSelect);
