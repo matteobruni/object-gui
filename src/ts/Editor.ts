07ed4392d964d61c6dd6da1a9b5b039c32269afe
Mitchell Sawatzky <s@watz.ky>
2020-10-06 18:40:54 -0600

    Add Nautilus theme

--- src/ts/Editor.ts
+++ src/ts/Editor.ts
@@ -41,6 +41,7 @@ export class Editor {
         this.addTheme("gold");
         this.addTheme("grey");
         this.addTheme("pink");
+        this.addTheme("nautilus");
 
         this.root = EditorGroup.createRoot(`${id}_editor`, name, data, document.body, this.themeSelect);
 
